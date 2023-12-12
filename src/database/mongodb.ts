a83b928b4efe6fbb851254359e2f17bd8733d3d4
João Eduardo <joao.silva@interativabr.com.br>
2023-12-12 11:23:02 -0300

    feat(Mongodb): initial config

---
+++ src/database/mongodb.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+class MongoDb {
+  static async connect() {
+    const username = process.env.MONGO_USERNAME || "";
+    const password = process.env.MONGO_PASSWORD || "";
+    const uri = process.env.MONGO_URI || "";
+
+    mongoose.set("strictQuery", true);
+
+    await mongoose.connect(uri, { auth: { password, username } });
+  }
+}
+
+export { MongoDb };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("ok");
+import "dotenv/config";
+import { MongoDb } from "./database/mongodb";
+import { Console } from "console";
+
+class Server {
+  static initializing() {
+    MongoDb.connect()
+      .then(() => {
+        console.log("connectado");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+}
+
+Server.initializing();
