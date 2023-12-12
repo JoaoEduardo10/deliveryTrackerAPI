4a392a84599025af7fe656b6178bb826b79d464d
João Eduardo <joao.silva@interativabr.com.br>
2023-12-12 13:14:10 -0300

    feat(express): initial config for router

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import { config } from 'dotenv';
+import { router } from './router';
 
 const app = express();
 config();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('ok');
-});
+app.use(`${process.env.VERSION}`, router);
 
 export { app };
