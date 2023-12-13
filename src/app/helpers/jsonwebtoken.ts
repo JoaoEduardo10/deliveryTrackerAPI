003546e92a9246b22eae054c11ef511de002695f
João Eduardo <joao.silva@interativabr.com.br>
2023-12-13 16:13:56 -0300

    feat(jwt): created functions

---
+++ src/app/helpers/jsonwebtoken.ts
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken';
+
+export interface Ijwt {
+  email: string;
+  name: string;
+  id: string | number;
+  jobs: string;
+}
+
+export interface IjwtComplete {
+  iat: number;
+  exp: number;
+}
+
+export const createJwt = (data: Ijwt) => {
+  const jwtHash = process.env.HASH_JWT as string;
+
+  return jwt.sign(data, jwtHash, { expiresIn: '24h' });
+};
+
+export const compareJwt = (
+  token: string,
+): (Ijwt & IjwtComplete) | undefined => {
+  const jwtHash = process.env.HASH_JWT as string;
+
+  try {
+    const varifyToken = jwt.verify(token, jwtHash);
+
+    if (typeof varifyToken === 'string') {
+      throw new Error('não autorizado');
+    }
+
+    return varifyToken as Ijwt & IjwtComplete;
+  } catch (error) {
+    if (error) return undefined;
+  }
+};
