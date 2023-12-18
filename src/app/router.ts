8f794fc14cc63c098eddca9bfcbc46acc4a9d7a4
João Eduardo <joao.silva@interativabr.com.br>
2023-12-18 14:46:19 -0300

    refactory: remove validation client

--- src/app/router.ts
+++ src/app/router.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import { CreateDeliveryMiddleware } from './middlewares/create-delivery';
 import { AuthenticationMiddleware } from './middlewares/authentication';
 import { CreateDeliveryRouter } from './usecase/create-delivery';
-import { IXC } from './services/ixc';
 
 const router = Router();
 
@@ -11,30 +10,6 @@ router.post(
   AuthenticationMiddleware.middleware,
   CreateDeliveryMiddleware.middleware,
   CreateDeliveryRouter.create,
-);
-
-router.get(
-  '/delivery',
-  AuthenticationMiddleware.middleware,
-  async (req, res) => {
-    const { cpf_cnpj } = req.query;
-
-    if (!cpf_cnpj) {
-      return res
-        .status(401)
-        .json({ error: 'Adicione o cpf ou cnpj para validação' });
-    }
-
-    const ixc = new IXC();
-
-    const client = await ixc.get_client({ cpf_cnpj: cpf_cnpj as string });
-
-    if (typeof client == 'string') {
-      return res.status(401).json({ error: 'CPF ou CNPJ invalido' });
-    }
-
-    res.status(200).json({ client: client[0] });
-  },
 );
 
 export { router };
