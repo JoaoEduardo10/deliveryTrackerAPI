24981e7e0c32d1e88a5651f0ee49e2b4fa18f996
João Eduardo <joao.silva@interativabr.com.br>
2023-12-28 12:08:01 -0300

    feat: adding limit

--- src/app/index.ts
+++ src/app/index.ts
@@ -10,7 +10,8 @@ import swaggerDocsJson from './swagger.json';
 
 const app = express();
 config();
-app.use(express.json());
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb' }));
 app.use(cors());
 
 app.use((req, res, next) => {
